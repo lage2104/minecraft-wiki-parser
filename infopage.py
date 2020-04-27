4f4c7e14a61b4eb9c14db901e7729760b38927ff
unknown <jan.quintus@web.de>
2020-04-27 10:23:30 +0200

    further improvements

--- infopage.py
+++ infopage.py
@@ -2,12 +2,9 @@ import re
 
 url = "https://minecraft.gamepedia.com/"
 
-def parseInfoBox(soup):
-  data = { "stack_size": 1 }
-  
+def parseInfoBox(data, soup):
+  data["stack_size"] = 1
   infobox_table = soup.find("table", {"class": "infobox-rows"})
-  
-
   for row in infobox_table.findAll("tr"):
     key = row.find("th").get_text().strip()
     value = row.find("td").get_text().strip()
@@ -16,5 +13,3 @@ def parseInfoBox(soup):
         data["stack_size"] = int(re.findall(r'\d+', value)[0])
       else:
         data["stack_size"] = 1
-  
-  return data
--- parser.py
+++ parser.py
@@ -2,27 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import blocknames
 import itemnames
-import infobox
+import infopage
 
 base_url = "https://minecraft.gamepedia.com/"
 
+# Retrive all block and item names (ex. Lava_Bucket)
 block_urls = blocknames.parseBlockNames()
 item_urls = itemnames.parseItemNames()
 data = []
 
+# Fetches an infopage for given url_name. url_name = element of block or item list /\
+# Returns soup object
 def fetchInfoPage(url_name):
   response = requests.get(base_url + url_name)
   html = response.text
   soup = BeautifulSoup(html,'html.parser')
   return soup
 
+# Here everything happens
 for block_url in block_urls:
   soup = fetchInfoPage(block_url)
-  block = infobox.parseInfoBox(soup)
-  print(block)
-  data.append(block)
+  # creating a first data dictionary with the name of the block
+  block_data = { "name": block_url.replace("_", " ") }
+  # parseInfoBox appends information to the block_data dict.
+  infopage.parseInfoBox(block_data, soup)
+  # add it to the final array
+  data.append(block_data)
+  print(block_data)
+
+# For items its the same procedure :D
 for item_url in item_urls:
   soup = fetchInfoPage(block_url)
-  item = infobox.parseInfoBox(item_url)
-  print(item)
-  data.append(item)
+  item_data = { "name": item_url.replace("_", " ") }
+  infopage.parseInfoBox(item_data, soup)
+  data.append(item_data)
+  print(item_data)
